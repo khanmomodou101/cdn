c4caa7c368313723f452117ea8ca6ca708112716
Momodou khan <125766896+khanmomodou101@users.noreply.github.com>
2024-03-11 18:25:50 +0000

    Create cdn.js

---
+++ cdn.js
@@ -0,0 +1,9 @@
+<script type="module">
+    import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+    Chatbot.init({
+        chatflowid: "cb1ac5d6-cc65-4f39-9552-a68cb2c816c1",
+        apiHost: "https://khanmomodou101-chatbot-flowise.hf.space",
+    })
+</script>
+
+
